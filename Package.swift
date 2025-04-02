2dbb8fc75a9d0212620c075fbe8594e5c4e0d767
nithuthms <nithuthms.official@gmail.com>
2025-04-02 19:49:31 -0400

    binaryTarget

--- Package.swift
+++ Package.swift
@@ -14,6 +14,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "IADNFramework",
+            path: "./Frameworks/IADNFramework.zip"
+        ),
         .target(
             name: "IADNLibrary"),
         .testTarget(
