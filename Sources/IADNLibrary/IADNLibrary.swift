d5a4418b81024bda79f93643ff53eb99d3a56d1c
nithuthms <nithuthms.official@gmail.com>
2025-04-02 11:43:49 -0400

    Initial commit

---
+++ Sources/IADNLibrary/IADNLibrary.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct MyFramework {
+    public static func greet() -> String {
+        return "Hello from MyFramework!"
+    }
+}
